368dcb3376c64ec801fd4abaa4c711eb4f9f3fe7
Mike <supahotboi69@gmail.com>
2026-08-10 23:38:42 -0400

    portraits for sakura empire shipgirls

--- devtools/create_live2d_portrait.py
+++ devtools/create_live2d_portrait.py
@@ -1,21 +1,38 @@
+import argparse
+import os
 import sys
 import pygame
 
-from ..engine.util import get_rect
-from ..live2d.live2d import Live2D
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-pygame.init()
-screen = pygame.display.set_mode((100,100))
+from engine.util import get_rect
+from live2d.live2d import Live2D, LAYER_SIZE
 
-shipgirl = sys.argv[1]
-live2d = Live2D(f"live2d/{shipgirl}.json")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Preview a shipgirl Live2D animation.")
+    parser.add_argument("--shipgirls", nargs="+", type=str, help="Shipgirl names, matching live2d/<shipgirl>.json")
+    args = parser.parse_args()
 
-sprite_size = 96
-sprite = pygame.Surface((sprite_size,sprite_size))
-sprite.fill((255,0,0))
-live2d.draw(sprite, sprite_size/2, sprite_size/2, False)
-portrait_size = 64
-crop = get_rect(width=portrait_size, height=portrait_size, centerx=sprite_size/2, top=0)
-portrait = sprite.subsurface(crop)
+    pygame.init()
+    screen = pygame.display.set_mode((100,100))
 
-pygame.image.save(portrait, f"live2d/{shipgirl}_portrait.png")
+    from src.constants import Box
+
+    num_shipgirls = len(args.shipgirls)
+    if num_shipgirls == 0:
+        sys.exit()
+
+    portraits = pygame.Surface((Box.WIDTH * num_shipgirls, Box.HEIGHT))
+    for i, shipgirl in enumerate(args.shipgirls):
+        live2d = Live2D(f"live2d/{shipgirl}.json")
+        sprite = pygame.Surface((LAYER_SIZE,LAYER_SIZE))
+        sprite.fill((255,0,0))
+        live2d.draw(sprite, LAYER_SIZE/2, LAYER_SIZE/2, False)
+        crop = get_rect(
+            width=Box.WIDTH, height=Box.HEIGHT,
+            centerx=LAYER_SIZE/2, top=0
+        )
+        portrait = sprite.subsurface(crop)
+        portraits.blit(portrait, (Box.WIDTH * i, 0))
+
+    pygame.image.save(portraits, f"live2d/portraits.png")
